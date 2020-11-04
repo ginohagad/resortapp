9ffd56efb254bb47201c4c3093254ea8125bb4fb
Gino <ginohagad@gmail.com>
2020-11-04 18:54:28 +1300

    Started creating Drawers (Material UI). Not finished.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,11 +1,8 @@
+import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
+import clsx from 'clsx';
+import { AppBar, Button, Drawer, Hidden, IconButton, List, ListItem, ListItemText, Toolbar, Typography } from '@material-ui/core/';
 import MenuIcon from '@material-ui/icons/Menu';
-
 import { Link } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
@@ -24,19 +21,37 @@ const useStyles = makeStyles((theme) => ({
 const Header = () => {
   const classes = useStyles();
 
+  const [state, setState] = React.useState({
+    menuBtn: false,
+  });
+
+  const toggleDrawer = (anchor, open) => event => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+    setState({ ...state, [anchor]: open });
+  };
+
+  // NOTE: Need to complete "Temporary Drawer" (Material UI)
+
+
   return (
     <div className="header">
       <AppBar position="static" color="primary">
 				<Toolbar>
-					<IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-						<MenuIcon />
-					</IconButton>
 					<Typography variant="h6" className={classes.title}>
 					  Resort App	
 					</Typography>
-          <Button color="inherit" component={Link} to="/">Home</Button>
-          <Button color="inherit" component={Link} to="/about">About</Button>
-          <Button color="inherit" component={Link} to="/services">Services</Button>
+          <Hidden xsDown>
+            <Button color="inherit" component={Link} to="/">Home</Button>
+            <Button color="inherit" component={Link} to="/about">About</Button>
+            <Button color="inherit" component={Link} to="/services">Services</Button>
+          </Hidden>
+          <Hidden smUp>
+            <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+              <MenuIcon />
+            </IconButton>
+          </Hidden>
 				</Toolbar>
 			</AppBar>
     </div>
